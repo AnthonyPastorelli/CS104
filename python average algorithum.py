764425867d28f0cae23433b53d90c3a6ac216cbf
AnthonyPastorelli <91150835+AnthonyPastorelli@users.noreply.github.com>
2021-10-05 00:13:10 -0400

    algorithm

---
+++ python average algorithum.py
@@ -0,0 +1,30 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> average=0
+>>> total=0
+>>> howmanyentered=0
+>>> input( "how many test scores would you like to average?" )
+how many test scores would you like to average?3
+'3'
+>>> howmany=3
+>>> input  ( "enter test score" )
+enter test score50
+'50'
+>>> testscore=50
+>>> total=0 + 50
+>>> input  ( "enter test score" )
+enter test score60
+'60'
+>>> total=total + 60
+>>> input  ( "enter test score" )
+enter test score70
+'70'
+>>> total=total + 70
+>>> average= total/ howmany
+>>> input ( "the average for the text score you entered  is: ")
+the average for the text score you entered  is: 60
+'60'
+>>> print ('average = 60')
+average = 60
+>>> 
