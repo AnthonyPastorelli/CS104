a1775df2c5e3585d0e57c2efb555f4910c98234d
AnthonyPastorelli <91150835+AnthonyPastorelli@users.noreply.github.com>
2021-12-13 22:26:26 -0500

    Queue code

---
+++ python Queue code.py
@@ -0,0 +1,9 @@
+NumofNames=0
+names = []
+for(NumofNames < 1)
+  newname=input('Nameplease!')
+  Names.append(newname)
+  NumofNames +=1
+for(NumofName > 9)
+  print (name.pop(NumofName))
+  NumofName -=1
