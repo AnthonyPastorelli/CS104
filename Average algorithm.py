3a36dfa14604dbc072e39941da8d3bbb3462bbf7
AnthonyPastorelli <91150835+AnthonyPastorelli@users.noreply.github.com>
2021-12-13 22:25:25 -0500

    new Average Algorithm

---
+++ Average algorithm.py
@@ -0,0 +1,13 @@
+set average to 0
+set total to 0
+set how many entered to 0
+output "how many test scores would you like to average?"
+howMany = int(input(''))
+output "enter test score: "
+testScore = int(input(''))
+total = total + testScore
+howManyEntered = howMany + 1
+if howManyEntered < howMany, repeat steps 6 thru 9
+average = total/howMany
+print ('The average for the test scores you entered is:')
+print ('average')
